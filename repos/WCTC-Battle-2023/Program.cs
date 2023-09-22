68ca705d0910fcd47697c4905da0aa3e0726850c
Josh Stueck <100880735+jstueck3@users.noreply.github.com>
2023-09-21 20:43:40 -0500

    Add files via upload

---
+++ repos/WCTC-Battle-2023/Program.cs
@@ -0,0 +1,26 @@
+﻿using Class05.Interfaces;
+using Class05.Models;
+
+namespace Class05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IArmor myNewArmor = myNewArmor();
+            IWeapon myNewWeapon = myNewWeapon();
+
+            IArmor armor = new Leather("Elven Leather Armor", 12);
+            IWeapon sword = new Sword("Long sword", 5);
+
+            ICombatant fighter = new Fighter("Fighter Fred", armor, sword);
+            ICombatant badGuy = new Fighter("Scary Monster", armor, sword);
+
+            fighter.Attack(badGuy);
+
+        }
+
+    }
+
+}
